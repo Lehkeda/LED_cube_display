aec2bf26de9077cfeb71b22390055ca6991c053c
Hesham El-Sewify <computer.boy71@yahoo.com>
2019-11-19 13:44:58 +0200

    add letters arrays and scroll to left animation for them

--- LED_cube_display.ino
+++ LED_cube_display.ino
@@ -1,5 +1,6 @@
 #include <FastLED.h>
 #include <SD.h>
+#include "letters.h"
 
 #define CHIP_SELECT 53
 #define DATA_OUT 51
@@ -11,7 +12,10 @@
 CRGB leds[NUM_LEDS];
 File frame;
 File temp;
-long int face_color_values[10][3]={
+bool current_letter_done= false;
+char *(letters)[256];
+
+long int current_letter[10][3]={
   {0,0,0},
   {0,0,0},
   {0,0,0},
@@ -24,8 +28,7 @@ long int face_color_values[10][3]={
   {0,0,0},
 };
 
-
-long int temp_face_color_values[10][3]{
+long int next_letter[10][3]={
   {0,0,0},
   {0,0,0},
   {0,0,0},
@@ -37,6 +40,48 @@ long int temp_face_color_values[10][3]{
   {0,0,0},
   {0,0,0},
 };
+
+void set_letters_array(){
+  letters['1']=number_1;
+  letters['2']=number_2;
+  letters['3']=number_3;
+  letters['4']=number_4;
+  letters['5']=number_5;
+  letters['6']=number_6;
+  letters['7']=number_7;
+  letters['8']=number_8;
+  letters['9']=number_9;
+  letters['0']=letter_o;
+  letters['a']=letter_a;
+  letters['b']=letter_b;
+  letters['c']=letter_c;
+  letters['d']=letter_d;
+  letters['e']=letter_e;
+  letters['f']=letter_f;
+  letters['g']=letter_g;
+  letters['h']=letter_h;
+  letters['i']=letter_i;
+  letters['j']=letter_j;
+  letters['k']=letter_k;
+  letters['l']=letter_l;
+  letters['m']=letter_m;
+  letters['n']=letter_n;
+  letters['o']=letter_o;
+  letters['p']=letter_p;
+  letters['q']=letter_q;
+  letters['r']=letter_r;
+  letters['s']=letter_s;
+  letters['t']=letter_t;
+  letters['u']=letter_u;
+  letters['v']=letter_v;
+  letters['w']=letter_w;
+  letters['x']=letter_x;
+  letters['y']=letter_y;
+  letters['z']=letter_z;
+  letters[' ']=space;
+  letters['-']=dash;
+  
+}
 
 void set_face_one(long int color, int row, int col){
   leds[((row*12)+col)]=color;
@@ -56,6 +101,104 @@ void set_faces_colors(long int color, int row, int col){
   set_face_two(color, row, col);
   set_face_three(color, row, col);
   set_face_four(color, row, col);
+}
+
+void set_current_letter_color(char letter){
+  for(char x=0; x<3; x++){
+    for(char y=9; y>=0; y--){
+      current_letter[y][x]=(bitRead(letters[letter][y], 2-x)? CRGB::Gold : 0);
+    }
+  }  
+}
+
+void set_next_letter_color(char letter){
+  for(char x=0; x<3; x++){
+    for(char y=9; y>=0; y--){
+      next_letter[y][x]=(bitRead(letters[letter][y], 2-x)? CRGB::Gold : 0);
+    }
+  }  
+}
+
+
+void show_on_cube(){
+  for(int x=0; x<3; x++){
+    for(int y=9; y>=0; y--){
+      set_faces_colors(current_letter[y][x], 9-y, x);
+    }
+  }
+
+  FastLED.show();
+  delay(70);
+  FastLED.clear();
+  delay(70);
+}
+
+void shift_and_dispay(){
+
+  show_on_cube();
+  
+  //shift 3rd col to 2nd col
+  for(int x=0; x <3; x++){
+    for(int y=9; y>=0; y--){
+      if(x<2){
+        current_letter[y][x]=current_letter[y][x+1];
+      }else if(x==2){
+        current_letter[y][x]=0;
+      }
+    }
+  }
+
+  show_on_cube();
+
+  // shift 2nd col to 1st col
+  for(int x=0; x <3; x++){
+    for(int y=9; y>=0; y--){
+      if(x<2){
+        current_letter[y][x]=current_letter[y][x+1];
+      }else if(x==1){
+        current_letter[y][x]=0;
+      }else if(x==2){
+         current_letter[y][x]=next_letter[y][0];
+      }
+    }
+  }
+
+  show_on_cube();
+
+  //set 0 col to 0
+  for(int x=0; x <3; x++){
+    for(int y=9; y>=0; y--){
+      if(x==0){
+        current_letter[y][x]=0;
+      }else if(x>=1){
+        current_letter[y][x]=next_letter[y][x-1];
+      }
+    }
+  }
+  show_on_cube();
+   
+  for(int x=0; x <3; x++){
+    for(int y=9; y>=0; y--){
+        current_letter[y][x]=next_letter[y][x];
+      }
+    }
+
+  show_on_cube();
+  
+}
+
+void parse_message(char msg[200]){
+  for(int i=0; msg[i]; i++){
+    set_current_letter_color(msg[i]);
+    if(!msg[i+1]){
+      set_next_letter_color(' '); 
+    }else{
+      set_next_letter_color(msg[i+1]);   
+    }
+    shift_and_dispay();
+  }
+//  current_letter_done
+  
 }
 
 void read_files(){
@@ -99,101 +242,31 @@ void read_files(){
   frame.close();
 }
 
-void read_into_array(){
-  temp = SD.open("display/n9.txt");
-  if (temp) {
-    String current_color = "";
-    int current_row=0;
-    int current_led_in_the_row=0;
-    while (temp.available()){
-      char color_value=temp.read();
-      if(color_value == '\n'){
-        if(current_led_in_the_row==3){
-          current_led_in_the_row= 0;
-          current_row++;
-        }
-
-        face_color_values[(9-current_row)][(2-current_led_in_the_row)]=current_color.toInt();
-        current_led_in_the_row++;
-        current_color="";
-      }else{
-        current_color += color_value;
-      }
-    } //frame.available()
-  } // frame
-  temp.close();
-}
-
-void show_on_cube(){
-  for(int x=0; x<3; x++){
-    for(int y=9; y>=0; y--){
-      set_faces_colors(temp_face_color_values[y][x], y, x);
-    }
-  }
-
-  FastLED.show();
-  delay(250);
-  FastLED.clear();
-  delay(250);
-}
-
-void shift_and_dispay(){
-  //copy to temporary array
-  for(int x=0; x<3; x++){
-      for(int y=9; y>=0; y--){
-        temp_face_color_values[y][x]=face_color_values[y][x];
-      }
-  }
-
-  show_on_cube();
-  
-  //shift 3rd col to 2nd col
-  for(int x=0; x <3; x++){
-    for(int y=9; y>=0; y--){
-      if(x<2){
-        temp_face_color_values[y][x]=temp_face_color_values[y][x+1];
-      }else if(x==2){
-        temp_face_color_values[y][x]=0;
-      }
-    }
-  }
-
-  show_on_cube();
-
-  // shift 2nd col to 1st col
-  for(int x=0; x <2; x++){
-    for(int y=9; y>=0; y--){
-      if(x<2){
-        temp_face_color_values[y][x]=temp_face_color_values[y][x+1];
-      }else if(x==1){
-        temp_face_color_values[y][x]=0;
-      }
-    }
-  }
-
-  show_on_cube();
-
-  //set 0 col to 0
-  for(int y=9; y>=0; y--){
-    temp_face_color_values[y][0]=0;
-  }
-
-  show_on_cube();
-}
-
 void setup() {
   FastLED.addLeds<WS2812B, CUBE_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(255);
   SD.begin();
   Serial.begin(115200);
+  set_letters_array();
 
 //  read_files();
 }
 
 
 void loop() {
-  read_into_array();
-  shift_and_dispay();
+  read_files();
+
+  delay(1500);
+  
+  parse_message("18-11-2019");
+  delay(500);
+  
+  parse_message("happy birthday to one of the nicest people i have ever met");
+
+  delay(1500);
+
+  parse_message("happy birthday enas");
+  delay(200);
 
 //    read_files();
 }
---
+++ letters.h
@@ -0,0 +1,78 @@
+#ifndef LETTERS_H
+#define LETTERS_H
+
+extern char number_1[10];
+
+extern char number_2[10];
+
+extern char number_3[10];
+
+extern char number_4[10];
+
+extern char number_5[10];
+
+extern char number_6[10];
+
+extern char number_7[10];
+
+extern char number_8[10];
+
+extern char number_9[10];
+
+extern char letter_a[10];
+
+extern char letter_b[10];
+
+extern char letter_c[10];
+
+extern char letter_d[10];
+
+extern char letter_e[10];
+
+extern char letter_g[10];
+
+extern char letter_f[10];
+
+extern char letter_h[10];
+
+extern char letter_i[10];
+
+extern char letter_j[10];
+
+extern char letter_k[10];
+
+extern char letter_l[10];
+
+extern char letter_m[10];
+
+extern char letter_n[10];
+
+extern char letter_o[10];
+
+extern char letter_p[10];
+
+extern char letter_q[10];
+
+extern char letter_r[10];
+
+extern char letter_s[10];
+
+extern char letter_t[10];
+
+extern char letter_u[10];
+
+extern char letter_v[10];
+
+extern char letter_w[10];
+
+extern char letter_x[10];
+
+extern char letter_y[10];
+
+extern char letter_z[10];
+
+extern char space[10];
+
+extern char dash[10];
+
+#endif //LETTERS_H
